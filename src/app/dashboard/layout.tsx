60fe6e58f9849c1fd284308495c94726ab7d840b
yishak621 <yishakabrham621@gmail.com>
2025-09-19 14:17:56 +0300

    progress-on-the-dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { ReactNode, useMemo } from "react";
-import { LayoutDashboard, Settings, Users, FileText } from "lucide-react";
+import { LayoutDashboard, Settings, Users, FileText, Bell } from "lucide-react";
 import DashboardShell, { NavItem } from "@/components/DashboardShell";
+import Image from "next/image";
 
 type Role = "admin" | "teacher";
 
@@ -14,13 +15,17 @@ function getSidebarItems(role: Role): NavItem[] {
       {
         label: "Curricular Area",
         icon: FileText,
-        href: "/dashboard/admin/reports",
+        href: "/dashboard/admin/curricular-area-management",
       },
-      { label: "Announcements", icon: Settings, href: "/dashboard/settings" },
+      {
+        label: "Announcements",
+        icon: Settings,
+        href: "/dashboard/announcements",
+      },
       {
         label: "Support Messages",
         icon: Settings,
-        href: "/dashboard/settings",
+        href: "/dashboard/support-messages",
       },
       { label: "App Settings", icon: Settings, href: "/dashboard/settings" },
     ];
@@ -38,12 +43,44 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
 
   const sidebarItems = useMemo(() => getSidebarItems(role), [role]);
 
+  const adminRightContent = () => {
+    return (
+      <div className="flex flex-row gap-4 ">
+        <div className=" flex justify-center items-center w-[44px] h-[44px] rounded-full bg-white relative">
+          <Bell className="w-5.5 h-5.5 text-[#0C0C0C]" />
+          <div className=" absolute bottom-0 right-0 w-[15px] h-[15px] bg-[#368FFF] rounded-full"></div>
+        </div>
+
+        <div className=" flex flex-row gap-2">
+          <div className=" flex flex-col justify-center items-center w-[51px] h-[51px] rounded-full bg-white">
+            <Image
+              className="w-11 h-11 rounded-full  border-2 border-[#368FFF] object-cover"
+              src="/images/sample-user.png"
+              alt="sample user image"
+              width={44}
+              height={44}
+            />
+          </div>
+
+          <div className="flex flex-col gap-[5px] border border-red">
+            <span className="font-base font-medium text-[#0C0C0C] ">
+              Admin user
+            </span>
+            <span className="text-sm font-normal text-[#717171]">
+              admin@school.com
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <DashboardShell
       title="Dashboard"
       sidebarItems={sidebarItems}
       onSearchChange={() => {}}
-      rightContent={<div>User Menu</div>}
+      rightContent={role === "admin" ? adminRightContent() : "dasda"}
       place={role === "admin" ? "admin" : "teacher"}
     >
       {children}
